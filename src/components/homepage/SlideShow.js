a04d707ab3e7da4c9cd9ca3ce17e352fa6c02492
Bemnet Dejene <bdejene19@gmail.com>
2021-03-22 17:32:15 -0400

    Added SlideShow effect to background

--- src/components/homepage/SlideShow.js
+++ src/components/homepage/SlideShow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import LandingPageHeader from './LandingPageHeader';
 
@@ -6,9 +6,8 @@ const SlideShowWrapper = styled.article`
     width: 100%;
     height: 100%;
     background-size: cover;
-     color: white;
-    
-
+    color: white;
+    /* background-image: url('concertLights.jpg'); */
 
     .slide-img {
         height: 100vh;
@@ -52,30 +51,33 @@ const SlideShowWrapper = styled.article`
     }
 `;
 
+
+
 export default function SlideShow() {
-    let myIndex = 0;
-    // const changeBackground = () => {
-    //       var x = document.getElementsByClassName("slide-img");
-    //       for (var count = 0; count < x.length; count++) {
-    //         x[count].style.display = "none";  
-    //       }
-    //       myIndex++;
-    //       if (myIndex > x.length) {myIndex = 1}    
-    //       x[myIndex-1].style.display = "block";  
-    //       setTimeout(changeBackground, 2000);    
-    // }
 
-    // changeBackground();
+    let backgroundImages = ['concertLights.jpg', 'secondConcert.jpg', 'thirdConcert.jpg'];
+    let count = 0;
+
+    useEffect(() => {
+        document.getElementById('slide-wrapper').style.backgroundImage = `url(${backgroundImages[count]})`;
+
+        setInterval(() => {
+            document.getElementById('slide-wrapper').style.backgroundImage = `url(${backgroundImages[count]})`;
+            document.getElementById('slide-wrapper').style.transition = "0.5s ease";
+
+            count++;
+
+            if (count === backgroundImages.length) {
+                count = 0;
+            }
+
+        }, 5000)
+    })
     return (
-        <SlideShowWrapper>
+        <SlideShowWrapper id='slide-wrapper'>
+            <LandingPageHeader></LandingPageHeader>
+
             <div id='slideShow'>
-                {/* <LandingPageHeader></LandingPageHeader> */}
-
-
-            {/* <img src="https://musicianonamission.com/wp-content/uploads/2018/06/samuel-zeller-103111-unsplash-800x533.jpg" alt="crowd" className='slide-img'></img> */}
-            {/* <img src="concertLights.jpg" alt="interactive concert" className='slide-img'></img>
-            <img src="leftStereo.png" alt="" className='slide-img'></img> */}
-
                 <div className='slideShowText'>
                     <h1>INTERACTIVE CONCERT EXPERIENCE</h1>
                     <p>Experience your favourite artists like never before and from the comfort of your own home.</p>
